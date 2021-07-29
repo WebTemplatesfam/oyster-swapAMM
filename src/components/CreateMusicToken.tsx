2862d47383bbd7da2655c45421e0bbc9cc136561
ProPratik1405 <sariapratik@gmail.com>
2021-07-29 18:30:42 +0530

    Added bg color because textfield are black somehow

--- src/components/CreateMusicToken.tsx
+++ src/components/CreateMusicToken.tsx
@@ -1,6 +1,7 @@
 import {Container,TextField,Button,Grid,} from "@material-ui/core"
 import {useState} from "react"
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { classicNameResolver } from "typescript";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -8,6 +9,9 @@ const useStyles = makeStyles((theme: Theme) =>
       color:"white",
       backgroundColor:"white"
       },
+    bg:{
+      backgroundColor:"white"
+    }
     },
 ));
 
@@ -20,20 +24,20 @@ export default function CreateMusicToken() {
 
   }
   return (
-    <Container fixed>
+    <Container fixed className={classes.bg}>
       <Grid container>
       <form>
       <Grid item xs={12}>
-      <TextField className={classes.input} required id="standard-required" label="Required" defaultValue="Song Name" value={data.song_name} onChange={(e:any) => setData({...data,song_name:e.target.value})} />
+      <TextField  required id="standard-required" label="Required" defaultValue="Song Name" value={data.song_name} onChange={(e:any) => setData({...data,song_name:e.target.value})} />
       </Grid>
       <Grid item xs={12}>
-      <TextField className={classes.input} required id="standard-required" label="Required" defaultValue="Author Name" value={data.author_name} onChange={(e:any) => setData({...data,author_name:e.target.value})} />
+      <TextField  required id="standard-required" label="Required" defaultValue="Author Name" value={data.author_name} onChange={(e:any) => setData({...data,author_name:e.target.value})} />
       </Grid>
       <Grid item xs={12}>
-      <TextField className={classes.input} required id="standard-required" label="Required" defaultValue="Public Address" value={data.public_address} onChange={(e:any) => setData({...data,public_address:e.target.value})} />
+      <TextField  required id="standard-required" label="Required" defaultValue="Public Address" value={data.public_address} onChange={(e:any) => setData({...data,public_address:e.target.value})} />
       </Grid>
       <Grid item xs={12}>
-      <TextField className={classes.input} required id="standard-required" label="Required" defaultValue="name of the Token" value={data.name_of_token} onChange={(e:any) => setData({...data,name_of_token:e.target.value})} />
+      <TextField  required id="standard-required" label="Required" defaultValue="name of the Token" value={data.name_of_token} onChange={(e:any) => setData({...data,name_of_token:e.target.value})} />
       </Grid>
       <Button onSubmit={handleSubmit}>Create new token</Button>
       <h2>{JSON.stringify(data)}</h2>
